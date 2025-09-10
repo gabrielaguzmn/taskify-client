92138a178d1f5339d97f96c34f307dc9d91ad55d
Gabriela Guzmán <136855829+gabrielaguzmn@users.noreply.github.com>
2025-09-10 07:44:48 -0500

    minor change in createTask function

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,24 +1,24 @@
 import { http } from "../api/http.js";
+import { getCurrentUser } from "../routes/route.js";
 
-export const taskService = {
-  getAll: () => http.get("/tasks"),
-  create: (task) => http.post("/tasks", task),
-  update: (id, task) => http.put(`/tasks/${id}`, task),
-  remove: (id) => http.del(`/tasks/${id}`)
-};
+// export const taskService = {
+//   getAll: () => http.get("/tasks"),
+//   // create: (task) => http.post("/tasks", task),
+//   // update: (id, task) => http.put(`/tasks/${id}`, task),
+//   // remove: (id) => http.del(`/tasks/${id}`)
+// };
 
-export async function createTask({ title, description, date, status }) {
+export async function createTask({ title, description, date, status, userId }) {
   // Get current user ID automatically
-  const currentUser = getCurrentUser();
-  if (!currentUser) {
-    throw new Error('User not logged in');
-  }
+  // const currentUser = getCurrentUser();
+  // if (!currentUser) {
+  //   throw new Error('User not logged in');
+  // }
 
   return http.post("/api/tasks/addTask", { 
     title, 
     description, 
     date, 
     status, 
-    userId: currentUser.id 
-  });
+    userId});
 }
